cd77572e28ed1cae4d47a8853abd407810c8ed8b
Simeon Visser <simeon87@gmail.com>
2015-01-01 03:36:37 +0100

    Added command for display contents of stored data

--- tracker/__init__.py
+++ tracker/__init__.py
@@ -7,12 +7,21 @@ __email__ = 'simeonvisser@gmail.com'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015 Simeon Visser'
 
+import os
+import shelve
+
 import click
 
 
 @click.command()
 def main():
-    click.echo("Hello")
+    try:
+        os.makedirs(os.path.expanduser("~/.tracker/"))
+    except FileExistsError:
+        pass
+    with shelve.open(os.path.expanduser("~/.tracker/data")) as d:
+        for item in d.items():
+            click.echo(item)
 
 
 if __name__ == '__main__':
